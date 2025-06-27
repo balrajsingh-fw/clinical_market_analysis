8202082fadcceb3b14a59aa84e8701115d92fd07
Balraj Singh Hada <balraj.singh@faberwork.com>
2025-06-27 17:42:17 +0530

    dockerfile fix

--- executable.sh
+++ executable.sh
@@ -5,7 +5,7 @@ echo "🔹 Apply migrations..."
 python manage.py makemigrations
 python manage.py migrate
 
-python import_script.py
-
 echo "🔹 Start Django server..."
 python manage.py runserver 0.0.0.0:8042
+
+python import_script.py
